524c3a6d601c68ba4f46b2c221bd1251fd08889b
Pankaj Singh <pankajsingh1397@gmail.com>
2024-01-12 18:09:13 +0530

    Update Speakers

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -4,10 +4,9 @@ function AboutUs() {
   return (
     <div className="flex flex-col justify-center items-center bg-[url('https://files.yappe.in/place/full/administration-block-iit-ropar-8176406.webp')] bg-cover">
         <div className="w-[90%] max-w-[1100px] lg:w-[80%] my-16 h-auto min-h-[90%]  flex flex-col
-             bg-gray-100/80 backdrop-brightness-75 px-20 py-10">
-
-            <h1 className="text-4xl pb-10">About Us</h1>
-            <p className="pb-10">The Indian Institute of Technology Ropar (IIT Ropar) is one of the eight new IITs
+             bg-gray-100/80 backdrop-brightness-75 px-4 md:p-8">
+            <h1 className="text-4xl py-5">About Us</h1>
+            <p className="pb-5">The Indian Institute of Technology Ropar (IIT Ropar) is one of the eight new IITs
                 established by the Ministry of Human Resource Development (MHRD), Government of India,
                 in 2009. Following the true tradition of the IIT system, the institute provides
                 state-of-the-art technical education across various fields and facilitates the
@@ -28,7 +27,7 @@ function AboutUs() {
                 a strong track record of placements, IIT Ropar emphasizes its dedication
                 to offering students a bright future and a wide array of career opportunities.
             </p>
-            <a  className='text-firstColor hover:text-thirdColor hover:underline' href='https://www.iitrpr.ac.in/'>Link to the Website</a>
+            <a  className='text-firstColor hover:text-thirdColor hover:underline py-5' href='https://www.iitrpr.ac.in/'>Link to the Website</a>
         </div>
 
     </div>
--- src/Pages/Home/CoreTeam.jsx
+++ src/Pages/Home/CoreTeam.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
             <center><img className="h-40" src={props.imageUrl}/>
             <br />
             <a className="text-blue-900 hover:text-blue-600" href="https://ctrg2023.trgindia.org/keynote_speakers.php">
-            <span className="text-md">Amitabh Bachchan</span>
+            <span className="text-md">Team Member Name</span>
             </a>
             </center>
         </div>
@@ -17,16 +17,16 @@ function KeyNoteSpeakers() {
         <div className="py-[40px] bg-thirdColor">
             <h3 className="text-center text-3xl pt-8"><b>Core Team</b></h3>
             <center><div className="w-[90%] max-w-[1100px] lg:w-[80%] grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5 sm:grid-cols-3 md:px-0 gap-6 pb-3 pt-8 ">
-            <Card imageUrl="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amitabh-bachchan-138-12-09-2017-02-34-37.jpg"/>
-            <Card imageUrl="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amitabh-bachchan-138-12-09-2017-02-34-37.jpg"/>
-            <Card imageUrl="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amitabh-bachchan-138-12-09-2017-02-34-37.jpg"/>
-            <Card imageUrl="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amitabh-bachchan-138-12-09-2017-02-34-37.jpg"/>
-            <Card imageUrl="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amitabh-bachchan-138-12-09-2017-02-34-37.jpg"/>
-            <Card imageUrl="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amitabh-bachchan-138-12-09-2017-02-34-37.jpg"/>
-            <Card imageUrl="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amitabh-bachchan-138-12-09-2017-02-34-37.jpg"/>
-            <Card imageUrl="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amitabh-bachchan-138-12-09-2017-02-34-37.jpg"/>
-            <Card imageUrl="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amitabh-bachchan-138-12-09-2017-02-34-37.jpg"/>
-            <Card imageUrl="https://in.bmscdn.com/iedb/artist/images/website/poster/large/amitabh-bachchan-138-12-09-2017-02-34-37.jpg"/>
+            <Card imageUrl="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png"/>
+            <Card imageUrl="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png"/>
+            <Card imageUrl="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png"/>
+            <Card imageUrl="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png"/>
+            <Card imageUrl="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png"/>
+            <Card imageUrl="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png"/>
+            <Card imageUrl="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png"/>
+            <Card imageUrl="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png"/>
+            <Card imageUrl="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png"/>
+            <Card imageUrl="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png"/>
             </div>
             </center>
         </div>
