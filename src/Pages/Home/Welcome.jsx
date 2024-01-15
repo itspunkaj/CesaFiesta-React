0eafd64479ec87aa5dfc23623f45f561d849b398
aditya955569 <aditya.kandpal2016@gmail.com>
2024-01-15 11:56:15 +0530

    updated notifications

--- src/Pages/Home/Welcome.jsx
+++ src/Pages/Home/Welcome.jsx
@@ -23,6 +23,7 @@ function Welcome() {
           <div className="text-3xl font-semibold">Notifications</div>
           <div className="my-10 text-xl leading-relaxed">
             <ul className="list-disc list-inside">
+            <li><a className="underline text-firstColor font-medium" href='https://drive.google.com/file/d/14yjOkd-korM4az6QzWiU9iJTavwhAvRT/view?usp=sharing' target='blank_'>Detailed Brochure</a></li>
               <li><a className="underline text-firstColor font-medium" href='https://drive.google.com/file/d/113gES1EJMbyKRWXVystK-q9_XwNoPHl9/view?usp=sharing' target='blank_'>Announcement Invitation</a></li>
               <li><a className="underline text-firstColor font-medium" href='https://drive.google.com/file/d/1zkMMuBF0aRp64Bf8JqZOoTLHBchT8bIB/view?usp=sharing' target='blank_'>Sponsorship Brochure</a></li>
             </ul>
