2a5e11963baebe0009f54e40ee446edad3bd4322
aditya955569 <aditya.kandpal2016@gmail.com>
2024-10-10 16:01:35 +0530

    updates

--- src/Pages/ImportantDates/ImportantDates.jsx
+++ src/Pages/ImportantDates/ImportantDates.jsx
@@ -5,7 +5,7 @@ function ImportantDates() {
           <div className="text-3xl md:text-4xl font-bold">Important Dates</div>
         </div>
         <div className="flex justify-center text-center px-10">
-          <table className="table-fixed border-collapse border-spacing-2 border-2 w-[800px] border-slate-300">
+          <table className="table-fixed border-collapse border-spacing-2 border-2 w-[1000px] border-slate-300">
             <thead>
               <tr className="bg-firstColor text-white">
                 <th className="md:text-2xl  py-2 px-3">Event</th>
@@ -19,11 +19,12 @@ function ImportantDates() {
               </tr>
               <tr className="hover:transform hover:scale-[1.02] hover:bg-[#f8f9fa] transition-transform duration-200 ease-in-out">
                 <td className="md:text-[18px] border-2 border-slate-300 py-4 px-3">Notification of Acceptance</td>
-                <td className="md:text-[18px] border-2 border-slate-300 py-4 px-3">1st week of November 2024</td>
+                <td className="md:text-[18px] border-2 border-slate-300 py-4 px-3">4th November 2024</td>
               </tr>
               <tr className="hover:transform hover:scale-[1.02] hover:bg-[#f8f9fa] transition-transform duration-200 ease-in-out">
                 <td className="md:text-[18px] border-2 border-slate-300 py-4 px-3">Opening of Online Registration</td>
-                <td className="md:text-[18px] border-2 border-slate-300 py-4 px-3">1st week of November 2024</td>
+                <td className="md:text-[18px] border-2 border-slate-300 py-4 px-3"><div><b>Early Bird offer</b> - 4th to 15th November 2024</div>
+                          <div><b>Regular fees</b> - 16th to 30th November 2024</div></td>
               </tr>
               <tr className="hover:transform hover:scale-[1.02] hover:bg-[#f8f9fa] transition-transform duration-200 ease-in-out">
                 <td className="md:text-[18px] border-2 border-slate-300 py-4 px-3">Symposium Date</td>
