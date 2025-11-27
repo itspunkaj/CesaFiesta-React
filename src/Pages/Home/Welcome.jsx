b6a803c6a69daa177cf3cd5f523ef32ee3cf3a9e
Ankit Raj <82669111+Ankitraj2003@users.noreply.github.com>
2025-11-27 22:51:33 +0530

    fix: correct date formatting and update registration fee dates in Welcome and ImportantDates components

--- src/Pages/Home/Welcome.jsx
+++ src/Pages/Home/Welcome.jsx
@@ -7,7 +7,7 @@ function Welcome() {
         <div className="col-span-12 md:col-span-8 pr-2">
           <div className="text-3xl font-semibold">Introduction</div>
           <div className="my-10 text-lg leading-relaxed justify-center items-center text-justify">The Department of Civil Engineering at the Indian Institute of Technology 
-            Ropar is all set to host the ‘Civil Engineering Student Association Fiesta’ (CESAFIESTA) for the third consecutive year on 21ᵗʰ & 22ᵗʰ February 2026. 
+            Ropar is all set to host the ‘Civil Engineering Student Association Fiesta’ (CESAFIESTA) for the third consecutive year on 21st & 22nd February 2026. 
             This 2-day symposium is attributed to the celebration of science, innovation and research in a diverse spectrum of civil engineering. This initiative 
             connects academia with the civil engineering sector, focusing on the confluence of cutting-edge technology and sustainable construction methodologies. 
             This initiative seeks to unite researchers from diverse sectors, cultivating their talents to collectively drive the nation's progress through skill-building, 
@@ -19,15 +19,15 @@ function Welcome() {
           <div className="my-10 text-lg leading-relaxed">
             <div className="flex flex-row items-center hover:thirdColor ">
               <FontAwesomeIcon icon={faBullhorn} className="h-[20px] w-[20px] text-firstColor"/>
-            <a className="text-firstColor font-medium hover:text-thirdColor ml-2" href='https://drive.google.com/file/d/1nYFKV734o2VLrM2vs1KL4NVHHevpm5k8/view?usp=drive_link' target='blank_'>Announcement</a>
+            <a className="text-firstColor font-medium hover:text-thirdColor ml-2" href='https://drive.google.com/file/d/1yTGO3E_ETUZSoRWF1_gv81d85gxBziT_/view?usp=drive_link' target='blank_'>Announcement</a>
             </div>
             <div className="flex flex-row items-center hover:thirdColor ">
               <FontAwesomeIcon icon={faBullhorn} className="h-[20px] w-[20px] text-firstColor"/>
-              <a className="ml-2 text-firstColor font-medium hover:text-thirdColor" href='https://drive.google.com/file/d/1pmrvFA667Otv0jnTz9nrqebh5s5pqs4a/view?usp=drive_link' target='blank_'>Detailed Brochure</a>
+              <a className="ml-2 text-firstColor font-medium hover:text-thirdColor" href='https://drive.google.com/file/d/1gBkvrR9Jwt5fKy0wrFdJVRpsEcveEsgq/view?usp=drive_link' target='blank_'>Detailed Brochure</a>
             </div>   
             <div className="flex flex-row items-center hover:thirdColor ">
               <FontAwesomeIcon icon={faBullhorn} className="h-[20px] w-[20px] text-firstColor"/>
-              <a className="ml-2 text-firstColor font-medium hover:text-thirdColor" href='https://drive.google.com/file/d/1rWBldeCanw8eC615Ev6IM6eFcQr3y4m-/view?usp=drive_link' target='blank_'>Sponsorship Brochure</a>
+              <a className="ml-2 text-firstColor font-medium hover:text-thirdColor" href='https://drive.google.com/file/d/1py4MHOPs-sWEVXwIcMdG8SFMaXVZQGYM/view?usp=drive_link' target='blank_'>Sponsorship Brochure</a>
             </div>    
            </div>   
         </div>
